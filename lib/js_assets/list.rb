f0f35a5629d727ccead9da163df3b71aaa53f37c
LucasK <lucas@clarityhk.com>
2015-12-16 13:04:38 +0800

    Add CDN support

--- lib/js_assets/list.rb
+++ lib/js_assets/list.rb
@@ -10,11 +10,14 @@ module JsAssets
       ::Rails.application.assets.logical_paths do |logical_path, filename|
         next if matches_filter(@exclude, logical_path, filename)
         next unless matches_filter(@allow, logical_path, filename)
+        
+        assets_root = ::Rails.application.config.action_controller.asset_host.present? ? ::Rails.application.config.action_controller.asset_host : '/'
+
         if ::Rails.application.assets.file_digest(filename)
-          project_assets[logical_path] = File.join('/', ::Rails.application.config.assets.prefix,
+          project_assets[logical_path] = File.join(assets_root, ::Rails.application.config.assets.prefix,
             ::Rails.application.assets[logical_path].digest_path)
         else
-          project_assets[logical_path] = File.join('/', ::Rails.application.config.assets.prefix,
+          project_assets[logical_path] = File.join(assets_root, ::Rails.application.config.assets.prefix,
             logical_path)
         end
       end
